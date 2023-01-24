eda964f0963ea16f822bb64bd865ea0334fc6ab4
alexisbarreaux <67143881+alexisbarreaux@users.noreply.github.com>
2023-01-24 17:23:18 +0100

    Added new instances

--- utils/constants.jl
+++ utils/constants.jl
@@ -4,5 +4,6 @@ RESULTS_DIR_PATH = dirname(UTILS_DIR_PATH) * "\\results"
 DATA_DIR_PATH = dirname(UTILS_DIR_PATH) * "\\data"
 
 DATA_FILES = ["taxe_grille_2x3.txt", "taxe_grille_3x4.txt", "taxe_grille_4x5.txt", "taxe_grille_5x6.txt",
-"taxe_grille_6x10.txt", "taxe_grille_6x7.txt", "taxe_grille_6x8.txt", "taxe_grille_6x9.txt",
-"taxe_grille_7x10.txt", "taxe_grille_7x11.txt", "taxe_grille_7x8.txt", "taxe_grille_7x9.txt"]
+"taxe_grille_6x10.txt","taxe_grille_6x11.txt", "taxe_grille_6x7.txt", "taxe_grille_6x8.txt", "taxe_grille_6x9.txt", 
+"taxe_grille_7x10.txt", "taxe_grille_7x11.txt", "taxe_grille_7x8.txt", "taxe_grille_7x9.txt",  "taxe_grille_8x10.txt", 
+"taxe_grille_8x9.txt"]
